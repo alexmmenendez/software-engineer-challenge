9abe05f6ee24ea9a88621d175bb5db1fdea720fd
Alex Menendez <alex.menendez@dasa.com.br>
2021-03-16 23:55:39 -0300

    init project

---
+++ src/main/java/com/picpay/softwareengineerchallenge/SoftwareEngineerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.softwareengineerchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEngineerChallengeApplication.class, args);
+	}
+
+}
