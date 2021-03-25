0cbe81b1ffb53541d49e17ef627d12cc32d9d79c
Alex Menendez <alex.menendez@dasa.com.br>
2021-03-25 03:33:24 -0300

    init mongodb scripts, dockerfile and docker-compose configuration

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM gradle:6.8.3-jdk11 as build
+COPY --chown=gradle:gradle . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle build -x test --no-daemon
+
+FROM openjdk:11-jre-slim
+
+EXPOSE 8080
+
+RUN mkdir /app
+
+COPY --from=build /home/gradle/src/build/libs/*.jar /app/picpay-api.jar
+
+ENTRYPOINT ["java", "-Djava.security.egd=file:/dev/./urandom","-jar","/app/picpay-api.jar"]
---
+++ mongoseed/import.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+awk -v RS='\r\n' 'BEGIN { OFS = "," } {print $0, "$2a$10$qqKXKdsDBWXMpvz0tMoq..qcTiir2Pif0y6VJ2lIpHIcQtunnh.pW, 0"}' users.csv > usersv2.csv
+sed '1iuuid, name, username, password, relevanceLevel' usersv2.csv > usersv3.csv
+mongoimport --host mongodb --type csv -d picpay -c user --headerline --drop usersv3.csv
+
+mongo --host mongodb < updateUserRelevanceLevel1.js
+mongo --host mongodb < updateUserRelevanceLevel2.js
